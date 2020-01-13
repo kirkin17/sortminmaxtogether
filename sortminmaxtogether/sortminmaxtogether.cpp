39a7b34f5cf95fc9833e8ff39e077e11c7160b8c
kirkin17 <kirkin17@users.noreply.github.com>
2020-01-13 16:22:45 +0300

    1

--- sortminmaxtogether/sortminmaxtogether.cpp
+++ sortminmaxtogether/sortminmaxtogether.cpp
@@ -1,20 +1,98 @@
-﻿// sortminmaxtogether.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <ctime>
+using namespace std;
 
-#include <iostream>
+void downtoup(int*);
+void uptodown(int*);
 
-int main()
+
+const int n = 10;
+void main()
 {
-    std::cout << "Hello World!\n";
+	setlocale(LC_ALL, "rus");
+	int a[n];
+	short int menu;
+
+
+	//random
+	while (1) {
+		srand(time(NULL));
+		for (int i = 0; i < n; i++)
+		{
+			a[i] = rand() % 20;
+		}
+		//output
+		for (int i = 0; i < n; i++)
+		{
+			cout << a[i] << "\t";
+		}
+		cout << endl;
+
+		cout << "Сортировка:\n";
+		cout << "1. По возрастанию\n";
+		cout << "2. По убыванию\n";
+		cin >> menu;
+
+		switch (menu)
+		{
+		case 1:
+		{
+			downtoup(a); break;
+		}
+		case 2:
+		{
+			uptodown(a); break;
+		}
+		}
+
+
+		for (int i = 0; i < n; i++)
+		{
+			cout << a[i] << "\t";
+		}
+		cout << endl;
+		system("pause");
+		system("cls");
+	}
+
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
 
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+//algorithm downtoup 
+void downtoup(int *a_ptr)
+{
+	for (int i = 0; i < n - 1; i++)
+	{
+		int min = i;
+		for (int j = i + 1; j < n; j++)
+		{
+			if (a_ptr[j] < a_ptr[min])
+				min = j;
+		}
+		if (min == i) continue;
+		int tmp = a_ptr[i];
+		a_ptr[i] = a_ptr[min];
+		a_ptr[min] = tmp;
+	}
+}
+
+
+
+
+//algorithm uptodown
+void uptodown(int *a_ptr)
+{
+	for (int i = 0; i < n - 1; i++)
+	{
+		int max = i;
+		for (int j = i + 1; j < n; j++)
+		{
+			if (a_ptr[j] > a_ptr[max])
+				max = j;
+		}
+		if (max == i) continue;
+		int tmp = a_ptr[i];
+		a_ptr[i] = a_ptr[max];
+		a_ptr[max] = tmp;
+	}
+}
